2a015324b8714eecc0190d5ade3da555978a38e5
medex81 <medex81@gmail.com>
2020-11-05 00:41:05 +0300

    ADD: Произведено разделение логики. Из обработчика UI, генерация полей перенесена в модуль логики жанрового движка.

--- Game/Scenes/GridContainer.gd
+++ Game/Scenes/GridContainer.gd
@@ -4,23 +4,29 @@ onready var field_red = preload("res://Game/Scenes/FieldRed.tscn")
 onready var field_blue = preload("res://Game/Scenes/FieldBlue.tscn")
 onready var field_yellow = preload("res://Game/Scenes/FieldYellow.tscn")
 onready var field_green = preload("res://Game/Scenes/FieldGreen.tscn")
+onready var scene_core = preload("res://Game/Logic/M3_core.gd").new()
 
 var n_field_size = 50
-var n_cols = 10
 var a_cells = []
-onready var a_fields_types = [field_red, field_blue, field_yellow, field_green]
 
-func _ready():
-	randomize()
-	#n_cols = rect_size.x / n_field_size
-	columns = n_cols
-	for ind in n_cols * n_cols:
-		var container = Container.new()
-		container.rect_min_size = Vector2(n_field_size, n_field_size)
-		a_cells.append(container)
-		add_child(container)
-		container.add_child(a_fields_types[randi() % a_fields_types.size()].instance())
-
+func _ready():	
+	scene_core.init()
+	if scene_core.n_cols != null:
+		columns = scene_core.n_cols
+		for ind in scene_core.fields_model:
+			var container = Container.new()
+			container.rect_min_size = Vector2(n_field_size, n_field_size)
+			a_cells.append(container)
+			add_child(container)
+			match ind:
+				scene_core.e_fields_types.EFT_BLUE:
+					container.add_child(field_blue.instance())
+				scene_core.e_fields_types.EFT_GREEN:
+					container.add_child(field_green.instance())
+				scene_core.e_fields_types.EFT_RED:
+					container.add_child(field_red.instance())
+				scene_core.e_fields_types.EFT_YELLOW:
+					container.add_child(field_yellow.instance())
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
