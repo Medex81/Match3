6a2ecd300ac76c50ac474e5a0b45b6ed2fab55ce
medex81 <medex81@gmail.com>
2021-01-22 00:28:34 +0300

    ADD: Добавлены менеджеры управления игрой и сервисами.

---
+++ Game/Scenes/FieldBlue.gd
@@ -0,0 +1,16 @@
+extends TextureRect
+
+var cb_click = null
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _on_Node2D_gui_input(event):
+	if event is InputEventMouseButton && event.button_index == BUTTON_LEFT && cb_click != null && !event.pressed:
+		cb_click.call_func(self.get_parent())
+
